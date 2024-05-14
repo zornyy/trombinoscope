431041408446da4f6e998cc67f5a58082fd2008d
Frédéric Shao <fredcshao@gmail.com>
2024-05-14 16:46:27 +0200

    Fix: endpoint auth check

--- Backend/pb_hooks/main.pb.js
+++ Backend/pb_hooks/main.pb.js
@@ -1,6 +1,4 @@
-$apis.requireRecordAuth(
-    routerAdd("GET", "/trombino", (c) => {
-        const id = c.get("authRecord").id
-        return c.json(200, { data: $app.dao().db().newQuery(`SELECT * FROM Trombino WHERE user_id = ${id}` )})
-    }
-))
+routerAdd("GET", "/trombino", (c) => {
+    if(!request.auth.id) return c.json(403, { data: null })
+    return c.json(200, { data: $app.dao().db().newQuery(`SELECT * FROM Trombino WHERE user_id = ${request.auth.id}` )})
+})

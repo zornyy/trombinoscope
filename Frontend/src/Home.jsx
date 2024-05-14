2e49f469cec05cd25a6a3c99957b6bb122c4696d
Loris Nanchen <loris.nanchen@edu.vs.ch>
2024-05-14 15:00:35 +0200

    Fetch in Home

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,24 +1,45 @@
 
 import { useContext, useEffect } from 'react'
 import './App.css'
+import { DBContext } from "./components/contexts/DBContext";
 import { useNavigate } from "react-router-dom"
 import { DBContext } from './components/contexts/DBContext'
 
 export default function Home() {
   const navigate = useNavigate();
   const {db} = useContext(DBContext);
+  const [records, setRecords] = useState([])
   const currentUser = db.authStore.model;
   
+  
+  const getRecords = async() => {
+    try {
+      const fetched = await db.collection('Picture').getFullList({
+        sort: '-created', requestKey: null 
+      });
+      setRecords(records)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    getRecords()
+  }, [])
+
   useEffect(() => {
     if (currentUser == null) {
       navigate("/login");
     }  
-  })
-  
+  }, [])
 
   return (
     <>
-      Hello
+      {
+        records.map(function(item, i) {
+          <img key={i} src={item.url}></img>
+        })
+      }
     </>
   )
 }

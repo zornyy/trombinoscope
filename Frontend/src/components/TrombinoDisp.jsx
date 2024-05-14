4fd98e657be508e2361ff081f793f69a126807d3
Zorn Alexandre <alexandre.zorn2005@gmail.com>
2024-05-14 16:40:56 +0200

    Added Trombino Display component -- Not finished implementing it

---
+++ Frontend/src/components/TrombinoDisp.jsx
@@ -0,0 +1,23 @@
+
+
+export default function TrombinoDisp() {
+
+
+
+    return (
+        <div className="border-solid border-2 border-white m-3 p-5 text-left max-w-80 rounded">
+            <div className="m-5 border-solid border-1 border-white">
+                <div className="font-bold">Name:</div>
+                Name of the trombino
+            </div>
+            <div className="m-5 border-solid border-1 border-white">
+                <div className="font-bold">Description:</div>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem placeat consectetur hic !
+            </div>
+            <div className="m-5 border-solid border-1 border-white text-blue-300">
+                <div className="font-bold">Owner:</div>
+                Zorny -_-
+            </div>
+        </div>
+    )
+}

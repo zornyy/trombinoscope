358317f470de9b131475447ca9d4b21713c3ef6b
Loris Nanchen <loris.nanchen@edu.vs.ch>
2024-05-14 15:01:56 +0200

    Fetch in Home

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -3,7 +3,6 @@ import { useContext, useEffect } from 'react'
 import './App.css'
 import { DBContext } from "./components/contexts/DBContext";
 import { useNavigate } from "react-router-dom"
-import { DBContext } from './components/contexts/DBContext'
 
 export default function Home() {
   const navigate = useNavigate();

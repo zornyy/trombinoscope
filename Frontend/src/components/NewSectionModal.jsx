03460d47e7d375017ee4b86f37716daf2dcc867d
Zorn Alexandre <alexandre.zorn2005@gmail.com>
2024-05-21 16:26:04 +0200

    Modal add section

--- Frontend/src/components/NewSectionModal.jsx
+++ Frontend/src/components/NewSectionModal.jsx
@@ -30,16 +30,16 @@ export default function NewSectionModal({show, onShowChanged, onSave=undefined})
         setDescription(e.target.value);
     }
     return (
-        <ModelBase title="Nouveau Trombino" show={show} onShowChanged={onShowChanged} onOk={handleClick}>
+        <ModelBase title="Nouvelle section" show={show} onShowChanged={onShowChanged} onOk={handleClick}>
             <div className="p-4 md:p-5 space-y-4">
                 <form className="max-w-sm mx-auto flex flex-col gap-4">
                     <div className="mb-5">
                         <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
-                        <input type="text" id="name" onChange={nameHandleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Administration" required />
+                        <input type="text" id="name" onChange={nameHandleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nom" required />
                     </div>
                     <div className="mb-5">
-                        <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                        <input type="text" id="username" onChange={descriptionHandleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                        <input type="text" id="description" onChange={descriptionHandleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     </div>
                 </form>
             </div>
--- Frontend/src/components/TrombinoDetail.jsx
+++ Frontend/src/components/TrombinoDetail.jsx
@@ -1,9 +1,11 @@
 import { useContext, useEffect, useState } from 'react'
 import '../App.css'
 import { DBContext } from "./contexts/DBContext";
+import NewSectionModal from './NewSectionModal';
 
 export default function TrombinoDetails({ id }) {
 
+	const [showCreateSectionModal, setShowCreateSectionModal] = useState(false)
 	const [record, setRecord] = useState([])
 	const {db} = useContext(DBContext);
 	db.autoCancellation(false);
@@ -18,6 +20,10 @@ export default function TrombinoDetails({ id }) {
 		}
 	}
 
+	const handleAddSection = () => {
+		setShowCreateSectionModal(true)
+	}
+
 	useEffect(() => {
     getRecords()
   }, [])
@@ -26,10 +32,28 @@ export default function TrombinoDetails({ id }) {
 		getRecords()
 	}, [id])
 
+	
+	if (id == "") {
+		return(
+			<div>
+				Sélectionnez un trobminoscope pour voir les détails :)
+			</div>
+		)
+	}
+
 	return(
 			<div>
+					<NewSectionModal show={showCreateSectionModal} onShowChanged={setShowCreateSectionModal}></NewSectionModal>
 					{record.name}<br></br>
 					{record.description}
+					
+					<button onClick={handleAddSection} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white dark:hover:bg-gray-700 group w-full hover:text-white hover:bg-[#646cff] duration-75">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+						</svg>
+
+						<span className="ms-3">Nouvelle section</span>
+					</button>
 			</div>
 	)
 }

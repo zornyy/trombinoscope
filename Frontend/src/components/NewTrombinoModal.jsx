2ea3e7df9e041e9490f0cc358f00f9b1d0ce4e01
Frédéric Shao <fredcshao@gmail.com>
2024-05-21 14:56:25 +0200

    Feat: new trombino modal finished

--- Frontend/src/components/NewTrombinoModal.jsx
+++ Frontend/src/components/NewTrombinoModal.jsx
@@ -1,42 +1,48 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import ModelBase from "./ModalBase"
+import { DBContext } from "./contexts/DBContext";
 
-export default function NewTrombinoModal() {
-    const [visible, setVisible] = useState(false)
+export default function NewTrombinoModal({show, onShowChanged, onSave=undefined}) {
+    const {db} = useContext(DBContext);
 
+    const [name, setName] = useState();
+    const [description, setDescription] = useState();
+
+    const handleClick = async() => {
+        const data = {
+            name: name,
+            description: description
+        };
+        try {
+            const record = await db.collection('Trombino').create(data);
+            onShowChanged(false)
+        }
+        catch(error) {
+            console.error(error);
+        }
+        
+    }
+
+    const nameHandleChange = (e) => {
+        setName(e.target.value);
+    }
+    const descriptionHandleChange = (e) => {
+        setDescription(e.target.value);
+    }
     return (
-        <div>
-            <button data-modal-target="default-modal" data-modal-toggle="default-modal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                Toggle modal
-            </button>
-            <div id="default-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div className="relative p-4 w-full max-w-2xl max-h-full">
-                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                Terms of Service
-                            </h3>
-                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                </svg>
-                                <span className="sr-only">Close modal</span>
-                            </button>
-                        </div>
-                        <div className="p-4 md:p-5 space-y-4">
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                            </p>
-                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                            </p>
-                        </div>
-                        <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                            <button data-modal-hide="default-modal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                            <button data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
-                        </div>
+        <ModelBase title="Nouveau Trombino" show={show} onShowChanged={onShowChanged} onOk={handleClick}>
+            <div className="p-4 md:p-5 space-y-4">
+                <form className="max-w-sm mx-auto flex flex-col gap-4">
+                    <div className="mb-5">
+                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
+                        <input type="text" id="name" onChange={nameHandleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Administration" required />
                     </div>
-                </div>
+                    <div className="mb-5">
+                        <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                        <input type="text" id="username" onChange={descriptionHandleChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                    </div>
+                </form>
             </div>
-        </div>
+        </ModelBase>
     )
 }

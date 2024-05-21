19724ad07703446c81d3634b658212b60ff80f35
Frédéric Shao <fredcshao@gmail.com>
2024-05-21 16:47:56 +0200

    Fix: new endpoint and hook

--- Backend/pb_hooks/main.pb.js
+++ Backend/pb_hooks/main.pb.js
@@ -1,4 +1,48 @@
+
+
 routerAdd("GET", "/trombino", (c) => {
-    if(!request.auth.id) return c.json(403, { data: null })
-    return c.json(200, { data: $app.dao().db().newQuery(`SELECT * FROM Trombino WHERE user_id = ${request.auth.id}` )})
+    const trombino = arrayOf(new DynamicModel({
+        "id": "",
+        "name": "",
+        "description": "",
+        "is_archived": false,
+        "user_id": ""
+    }))
+
+    const record = c.get("authRecord");
+    if(!record) return c.json(403, { data: null })
+
+    $app.dao().db()
+        .select("*")
+        .from("Trombino")
+        .where($dbx.exp("user_id = {:id}", { id: record.id }))
+        .all(trombino)
+
+    return c.json(200, trombino)
 })
+
+onModelBeforeCreate((e) => {
+    const record = e.get("authRecord");
+    if(!record) return c.json(403, { data: null })
+    e.model.tableName()
+    if(e.model.is_archived == undefined) e.model.is_archived = false
+    e.model.user_id = false
+}, "Trombino")
+
+onRecordViewRequest((e) => {
+    const sections = arrayOf(new DynamicModel({
+        "id": "",
+        "name": "",
+        "description": "",
+        "trombino_id": ""
+    }))
+    console.log(0)
+    console.log(JSON.stringify(e.httpContext))
+    $app.dao().db()
+        .select("*")
+        .from("Section")
+        .where($dbx.exp("trombino_id = {:id}", { id: e.record.id }))
+        .all(sections)
+    e.record.sections = []
+    console.log(JSON.stringify(e.record))
+}, "Trombino")

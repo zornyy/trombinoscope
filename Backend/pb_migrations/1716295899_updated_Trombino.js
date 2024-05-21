dc37cca546b821f92acf231ff657a41b9cdcbca7
Frédéric Shao <fredcshao@gmail.com>
2024-05-21 15:00:56 +0200

    Fix: Trombino

---
+++ Backend/pb_migrations/1716295899_updated_Trombino.js
@@ -0,0 +1,104 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("5u3vl9u558rybny")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ouqfvzpo",
+    "name": "name",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bgqpsvyo",
+    "name": "is_archived",
+    "type": "bool",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "2yyabjss",
+    "name": "user_id",
+    "type": "relation",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("5u3vl9u558rybny")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ouqfvzpo",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bgqpsvyo",
+    "name": "is_archived",
+    "type": "bool",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "2yyabjss",
+    "name": "user_id",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "_pb_users_auth_",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": 1,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})

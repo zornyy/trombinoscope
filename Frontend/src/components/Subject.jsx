14ed7059dc01cc205d0632dda66a07883840f41f
Frédéric Shao <fredcshao@gmail.com>
2024-05-28 10:09:09 +0200

    Fix: image_url

--- Frontend/src/components/Subject.jsx
+++ Frontend/src/components/Subject.jsx
@@ -1,10 +1,11 @@
+import { useContext } from 'react'
 import '../App.css'
+import { DBContext } from './contexts/DBContext'
 
 export default function Subject({ subject }) {
-
     return (
         <div>
-            <div>{subject.image}</div>
+            <img src={`${import.meta.env.VITE_BACKEND_URL}/api/files/Subject/${subject.id}/${subject.image}`} />
             <div>{subject.formatted_name}</div>
         </div>
     )

ba2513d49e60360decc9ed801d094a9c16aaf42c
Zorn Alexandre <alexandre.zorn2005@gmail.com>
2024-05-28 16:18:01 +0200

    Updated image selection

--- Frontend/src/components/NewSubjectModal.jsx
+++ Frontend/src/components/NewSubjectModal.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import ModalBase from "./ModalBase"
 import { DBContext } from "./contexts/DBContext";
 
@@ -9,9 +9,17 @@ export default function NewSubjectModal({ show, onShowChanged, sections, onSave 
     const [description, setDescription] = useState("");
     const [section, setSection] = useState("");
     const [image, setImage] = useState();
+    const [isMobile, setIsMobile] = useState(false);
 
     const { db } = useContext(DBContext)
 
+    useEffect(() => {
+        const checkIfMobile = () => {
+          return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+        };
+        setIsMobile(checkIfMobile());
+      }, []);
+ 
     const handleSubmit = async () => {
         const data = {
             first_name: firstName,
@@ -32,6 +40,7 @@ export default function NewSubjectModal({ show, onShowChanged, sections, onSave 
         }
 
     }
+      
     return (
         <ModalBase title="Ajouter un sujet" show={show} onShowChanged={onShowChanged} onOk={handleSubmit}>
             <form className="max-w-sm mx-auto flex flex-col gap-4">
@@ -60,9 +69,15 @@ export default function NewSubjectModal({ show, onShowChanged, sections, onSave 
                         {sections.map((x) => <option value={x.id} key={x.id}>{x.name}</option>)}
                     </select></div>
                 <div className="mb-5">
-                    <label htmlFor="image" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image :</label>
-                    <input type="file" accept="image/*" capture="environment" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setImage(e.target.files[0])} />
+                    <label htmlFor="image" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a picture:</label>
+                    <input type="file" accept="image/*" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setImage(e.target.files[0])} />
                 </div>
+                {isMobile ? (
+                    <div className="mb-5">
+                        <label htmlFor="image" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Take a picture:</label>
+                        <input type="file" accept="image/*" capture="environment" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setImage(e.target.files[0])} />
+                    </div>
+                ) : (<></>)}
             </form>
         </ModalBase>
     )

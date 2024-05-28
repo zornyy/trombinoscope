c15b865beea28a69f27a17f79ef5679dcbde1710
Frédéric Shao <fredcshao@gmail.com>
2024-05-28 15:21:57 +0200

    Feat: Grid

--- Frontend/src/components/SectionDetails.jsx
+++ Frontend/src/components/SectionDetails.jsx
@@ -4,9 +4,9 @@ import Subject from './Subject'
 export default function SectionDetails({ section }) {
 
     return (
-        <div>
-            <div>{section.name}</div>
-            <div>{section.subjects.map((x) => <Subject subject={x} key={x.id} />)}</div>
+        <div className="flex flex-col items-start">
+            <h2 className="mb-2 text-xl font-medium text-gray-900 dark:text-white">{section.name}</h2>
+            <div className="w-full grid grid-cols-3 md:grid-cols-5 gap-4">{section.subjects.map((x) => <Subject subject={x} key={x.id} />)}</div>
         </div>
     )
 }
